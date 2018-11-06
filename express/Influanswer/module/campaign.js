b921fccfaa0c75c551c572e6ac0ad8c34437b4b8
KIMSJ <leesd556@gmail.com>
2018-11-06 15:30:40 +0900

    modify search module to return several values

--- express/Influanswer/module/campaign.js
+++ express/Influanswer/module/campaign.js
@@ -3,18 +3,28 @@ const db = require('./db')
 module.exports = {
   click : async (types) => {
       let returnData = new Array()
+      let selectContentQuery = ""
 
-      let selectContentQuery =
-      `
-      SELECT *
-      FROM campaign
-      WHERE type IN
-      `
-      for(i in types){
-        types[i] = JSON.stringify(types[i])
+      if(types.length == 0){ // types가 비어있으면 전체 호출
+        selectAllQuery =
+        `
+        SELECT *
+        FROM campaign
+        `
+      } else {
+        selectContentQuery =
+        `
+        SELECT *
+        FROM campaign
+        WHERE type IN
+        `
+        for(i in types){
+          types[i] = JSON.stringify(types[i])
+        }
+
+        selectContentQuery += "(" + types.join() + ")"
       }
 
-      selectContentQuery += "(" + types.join() + ")"
       let selectContentResult = await db.queryParamNone(selectContentQuery)
       if(!selectContentResult){
         throw "500"
@@ -39,33 +49,51 @@ module.exports = {
   },
 
   search : async (types, keyword) => {
-    let returnData = new Object()
+    let returnData = new Array()
+    let selectContentQuery = ""
 
-    let selectContentQuery =
-    `
-    SELECT *
-    FROM campaign
-    WHERE type IN
-    `
-    for(i in types){
-      types[i] = JSON.stringify(types[i])
+    if(types.length == 0){
+      selectContentQuery =
+      `
+      SELECT *
+      FROM campain
+      WHERE name LIKE "%"?"%"
+      `
+    } else {
+      selectContentQuery =
+      `
+      SELECT *
+      FROM campaign
+      WHERE type IN
+      `
+      for(i in types){
+        types[i] = JSON.stringify(types[i])
+      }
+
+      selectContentQuery += "(" + types.join() + ")"
+      selectContentQuery +=
+      `
+      AND name LIKE "%"?"%"
+      `
     }
 
-    selectContentQuery += "(" + types.join() + ")"
-    selectContentQuery +=
-    `
-    AND name LIKE "%"?"%"
-    `
     let selectContentResult = await db.queryParamArr(selectContentQuery, keyword)
     if(!selectContentResult){
       throw "500"
     }
-    returnData.campaign_id = selectContentResult[0].campaign_id
-    returnData.name = selectContentResult[0].name
-    returnData.type = selectContentResult[0].type
-    returnData.reward = selectContentResult[0].reward
-    returnData.description = selectContentResult[0].description
-    returnData.image = selectContentResult[0].image
+
+    for(i in selectContentResult){
+      let form = new Object()
+
+      form.campaign_id = selectContentResult[i].campaign_id
+      form.name = selectContentResult[i].name
+      form.type = selectContentResult[i].type
+      form.reward = selectContentResult[i].reward
+      form.description = selectContentResult[i].description
+      form.image = selectContentResult[i].image
+
+      returnData.push(form)
+    }
 
     return returnData
   }
