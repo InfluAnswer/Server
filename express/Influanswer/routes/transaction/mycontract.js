c441284eb16cd994f2bbf5e937e86f24fb3e575a
KIMSJ <leesd556@gmail.com>
2018-11-18 22:26:15 +0900

    Add mycontract

---
+++ express/Influanswer/routes/transaction/mycontract.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const router = express.Router()
+const transactionModule = require('../../module/transaction.js')
+const verify = require('../../module/user').verify
+
+router.post('/', async(req, res, next) => {
+  let user
+    try{
+      user = await verify(req.headers.token)
+    } catch(err){
+      next(err)
+      return
+    }
+  let returnData
+
+  try {
+  	if(!user){
+  		next("400")
+  		return
+  	}
+
+    if(user.index == 0){
+      returnData = await transactionModule.mycontract_inf(user.id)
+    } else{
+      returnData = await transactionModule.mycontract_adv(user.id)
+    }
+
+  } catch(err) {
+  	next(err)
+  	return
+  }
+
+  res.r(returnData)
+})
+
+module.exports = router
